ac79dfc7d86dc97950233756a157f3dd021fe730
Rahul Saha <rahulsaha87928@gmail.com>
2023-09-14 18:02:58 +0600

    create api for get single product id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 require('dotenv').config()
 const app = express();
@@ -15,44 +15,64 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
 
-    const productCollection = client.db('techTroveDB').collection("products");
+        const productCollection = client.db('techTroveDB').collection("products");
+        const CartCollection = client.db('techTroveDB').collection("carts");
 
-    //get all products from Product Collection
-    app.get('/products', async (req, res) => {
-        const result = await productCollection.find().toArray();
-        res.send(result)
-    })
-
-    //post product to the Product Collection
-    app.post('/products', async (req, res) => {
-        const product = req.body;
-        console.log('new product', product);
-        const result = await productCollection.insertOne(product);
-        res.send(result);
-    })
+        //------------- Product Collection Section -------------------
 
 
+        //get all products from Product Collection
+        app.get('/products', async (req, res) => {
+            const result = await productCollection.find().toArray();
+            res.send(result)
+        })
+
+        //get single product from Product Collection
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await productCollection.findOne(query);
+            res.send(result);
+        })
+
+        //post product to the Product Collection
+        app.post('/products', async (req, res) => {
+            const product = req.body;
+            console.log('new product', product);
+            const result = await productCollection.insertOne(product);
+            res.send(result);
+        })
 
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        //------------- Cart Collection Section -------------------
+
+        // post products from Cart Collection
+        app.post("/cartProducts", async (req, res) => {
+            const cartItem = req.body;
+            const result = await CartCollection.insertOne(cartItem);
+            res.send(result);
+        });
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
